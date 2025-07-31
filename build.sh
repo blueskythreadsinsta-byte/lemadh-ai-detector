7d71d251c9202a7435653197d70fbe5b4f16fd8c
blueskythreadsinsta-byte <blueskythreadsinsta@gmail.com>
2025-08-01 00:44:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
-#!/usr/bin/env bash
-python -m nltk.downloader punkt
+#!/bin/bash
+
+# Step 1: Install dependencies
+pip install -r requirements.txt
+
+# Step 2: Download NLP models
 python -m spacy download en_core_web_sm
+python -m nltk.downloader punkt
